9c94fc634e530a961f5e96a1f15251346e614e1c
Berezovsky Renat <brendy@brenat.varnoff.net>
2016-12-19 15:28:01 -0500

    complete input mask

--- src/js/callback.js
+++ src/js/callback.js
@@ -15,7 +15,6 @@ const modal = `<div class="cb-bg"></div> \
                           <div class="form-group"> \
                             <label class="control-label" for="phone">Телефон</label> \
                             <input type="phone" title="Введите ваш номер" required="" value="" name="phone" id="phone" class="form-control"> \
-                            <span id="phoneMask" class="mask">+7 (___) ___ - __ - __</span> \
                           </div> \
                           <button class="btn btn--green">Отправить</button> \
                         </form> \
@@ -27,6 +26,7 @@ const modalClose = `<button id="close" class="btn btn--red">Close callback</butt
 
 const CallBack = function(target) {
   this.target = document.querySelector(target);
+  this.mask = `+ 7 (___) ___ - __ - __`;
 };
 
 CallBack.prototype = {
@@ -56,33 +56,90 @@ CallBack.prototype = {
   },
   inputListener: function() {
     let inputPhone = this.cbBody.querySelector('#phone');
-    inputPhone.addEventListener('keypress',(e) => {
-      let val = e.target.value.length;
-      if (val <= 17) {
-        if (e.keyCode >= 48 && e.keyCode <= 57) {
-          e.target.classList.contains('error') ? e.target.classList.remove('error') : null;
-          if (val === 3) {
-            e.target.value += String.fromCharCode(160) + String.fromCharCode(160)
-          }
-          if (val === 8 || val === 13) {
-            e.target.value += String.fromCharCode(160) + String.fromCharCode(160) + String.fromCharCode(160)
-          }
-          if (val === 17) {
-            e.target.classList.add('success')
-          }
-        } else {
-          e.target.classList.contains('error') ? null :  e.target.classList.add('error');
-          e.preventDefault()
-        }
-      } else {
-        e.preventDefault()
-      }
-    })
+    inputPhone.setAttribute('placeholder',this.mask);
+    this.applyDataMask(inputPhone, this.mask);
   },
   open: function() {
     document.body.insertBefore(this.cbBody, document.body.firstChild);
   },
   close: function() {
     this.cbBody.remove();
+  },
+  setCursor: function (elem, caretPos) {
+    if(elem.createTextRange) {
+      let range = elem.createTextRange();
+      range.move('character', caretPos);
+      range.select();
+    }
+    else {
+      if(elem.selectionStart) {
+        elem.focus();
+        elem.setSelectionRange(caretPos, caretPos);
+      }
+      else
+        elem.focus();
+    }
+  },
+  applyDataMask: function(field, mask) {
+
+    var mask = mask.split('');
+
+    // For now, this just strips everything that's not a number
+    let stripMask = (maskedData) => {
+      function isDigit(char) {
+        return /\d/.test(char);
+      }
+      return maskedData.split('').filter(isDigit);
+    }
+
+    // `_` characters => characters from `data`
+    let applyMask = (data) =>
+      mask.map(function(char, index) {
+        if (char != '_' && index != 2) return char;
+        if (data.length == 0) return char;
+        return data.shift();
+      }).join('')
+
+    let reapplyMask = (data) => {
+      return applyMask(stripMask(data))
+    }
+
+    let smartfocus = (flag) => {
+      let oldStart = field.selectionStart;
+      let oldEnd = field.selectionEnd;
+      field.value = reapplyMask(field.value);
+
+      if (field.value.indexOf('_') >= 0 && flag) {
+                this.setCursor(field, field.value.indexOf('_'));
+      } else if (!flag) {
+        field.selectionStart = oldStart + 1;
+        field.selectionEnd = oldStart;
+      } else if (!(field.value.indexOf('_') < 0) && flag) {
+        field.selectionStart = oldStart + 1;
+        field.selectionEnd = oldStart + 1;
+      }
+    }
+
+    field.addEventListener('click', (e) => {
+      if ( /\d/.test(e.target.value.charAt(field.selectionStart - 1)) ||  /\d/.test(e.target.value.charAt(field.selectionStart))) {
+        smartfocus(false)
+      } else {
+        smartfocus(true)
+      }
+    });
+    field.addEventListener('keydown', (e) => {
+      if (!(e.keyCode >= 48 && e.keyCode <= 57) && e.keyCode !== 8 && e.keyCode !== 46) {
+        e.preventDefault()
+      }
+    });
+    field.addEventListener('keyup', (e) => {
+      if (e.keyCode == 8) {
+        smartfocus(false)
+      } else if (e.keyCode >= 48 && e.keyCode <= 57){
+        smartfocus(true)
+      } else {
+        e.preventDefault()
+      }
+    })
   }
 }
