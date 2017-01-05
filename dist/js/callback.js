8031ed7a10ec0fd59166b34b62eb105b2247a894
brrenat <renat0014@gmail.com>
2017-01-06 00:27:03 +0300

    dist and separate js and css file for static and iframe page

---
+++ dist/js/callback.js
@@ -0,0 +1,211 @@
+'use strict';
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+/**
+ * Created by BRenat on 04.01.2017.
+ */
+var defaults = {
+  imgClose: 'img/close.png',
+  imgLogo: 'http://localhost:8080/img/logo.png',
+  color: {
+    first: '#f49a00',
+    second: '#fab900',
+    phone: '#3CB868'
+  },
+  mask: '+ 7 (___) ___ - __ - __',
+  position: 'bottom:20px; left:20px;'
+};
+
+var iframeStyle = 'body{font-family:PT Sans,sans-serif;margin:0;padding:0}.br-logo{text-align:center}.br-logo img{max-width:100%;max-height:100%}.br-text{text-align:center;padding-top:20px;padding-bottom:25px}.br-form__group{margin-bottom:15px}.br-form__group,.br-form__input{position:relative;text-align:center}.br-form__input{display:block;width:100%;max-width:370px;padding:9px 12px 7px;font-size:25px;line-height:1.42857143;color:#767676;margin:0 auto 20px;background:#fff;box-shadow:0 0 4.8px 0.3px rgba(0,0,0,.2);border:1px solid #ccc;-webkit-transition:all .15s ease-in-out;transition:all .15s ease-in-out;z-index:2}.br-form__input.success{border-color:green}.br-form__input#phone{text-align:left;letter-spacing:2px}.br-form__btn{border-radius:5px;display:block;padding:14px 50px 13px;font-size:18px;font-weight:400;text-align:center;vertical-align:middle;cursor:pointer;-webkit-user-select:none;-moz-user-select:none;-ms-user-select:none;user-select:none;margin:40px auto 10px;color:#fff;background:#3cb868;border:none;text-transform:uppercase;background-size:100% 100%;text-shadow:0 0 4.8px rgba(0,0,0,.2)}';
+
+var iframeScript = 'var inputPhone = document.querySelector(\'#phone\');console.log(inputPhone)';
+
+var CallMe = function () {
+  function CallMe(options) {
+    _classCallCheck(this, CallMe);
+
+    this.options = Object.assign({}, defaults, options);
+    this.elements = {
+      modalClassNames: {
+        bg: 'br-bg',
+        wrap: 'br-wrap',
+        container: 'br-container',
+        content: 'br-content',
+        form: 'br-form',
+        open: 'cback'
+      },
+      modalBody: '<div class="br-container"><div class="br-content"><iframe class="br-iframe" name="frameForm" id="frameForm" src="" allowfullscreen></iframe></div></div>',
+      modalClose: '<button class="br-close"><img src=' + this.options.imgClose + ' alt=""></button>',
+      modalOpen: '<div class="cback-circle fn1"></div><div class="cback-circle fn2"></div><div class="cback-circle cback-circle--phone"><i class=\'phone-icon\'></i></div>',
+      modalStyle: '.cback-circle {color: ' + this.options.color.phone + '; background: ' + this.options.color.phone + ';} .br-form__btn {background: ' + this.options.color.first + '} .br-form__btn:hover {background: ' + this.options.color.second + '}',
+      modalIframe: '<div class="br-logo">\n\t\t\t\t\t\t<img src=' + this.options.imgLogo + ' alt="">\n\t\t\t\t\t</div>\n\t\t\t\t\t<div class="br-text">\u0417\u0430\u043A\u0430\u0436\u0438\u0442\u0435 \u043E\u0431\u0440\u0430\u0442\u043D\u044B\u0439 \u0437\u0432\u043E\u043D\u043E\u043A, <br> \u043C\u044B \u043F\u0435\u0440\u0435\u0437\u0432\u043E\u043D\u0438\u043C \u0412\u0430\u043C \u0432 \u0431\u043B\u0438\u0436\u0430\u0439\u0448\u0435\u0435 \u0432\u0440\u0435\u043C\u044F!</div>\n\t\t\t\t\t<div class="br-form">\n\t\t\t\t\t\t<form action="#">\n\t\t\t\t\t\t\t<div class="br-form__group">\n\t\t\t\t\t\t\t\t<input type="text" placeholder="\u0412\u0430\u0448\u0435 \u0438\u043C\u044F" title="\u0412\u0432\u0435\u0434\u0438\u0442\u0435 \u0432\u0430\u0448\u0435 \u0438\u043C\u044F" required="" value="" name="username" id="username" class="br-form__input">\n\t\t\t\t\t\t\t</div>\n\t\t\t\t\t\t\t<div class="br-form__group">\n\t\t\t\t\t\t\t\t<input type="phone" title="\u0412\u0432\u0435\u0434\u0438\u0442\u0435 \u0432\u0430\u0448 \u043D\u043E\u043C\u0435\u0440" required="" value="" name="phone" id="phone" class="br-form__input">\n\t\t\t\t\t\t\t</div>\n\t\t\t\t\t\t</form>\n\t\t\t\t\t</div>\n\t\t\t\t\t<button class="br-form__btn">\u0416\u0434\u0443 \u0437\u0432\u043E\u043D\u043A\u0430</button>'
+    };
+    this.init();
+  }
+
+  _createClass(CallMe, [{
+    key: 'init',
+    value: function init() {
+      this.render();
+      this.clickHandler();
+      // this.inputListener();
+    }
+  }, {
+    key: 'clickHandler',
+    value: function clickHandler() {
+      var _this = this;
+
+      this.cbOpen.addEventListener('click', function (e) {
+        _this.open();
+        e.preventDefault();
+      });
+      this.cbClose.addEventListener('click', function (e) {
+        _this.close();
+        e.preventDefault();
+      });
+    }
+  }, {
+    key: 'render',
+    value: function render() {
+      this.cbStyle = document.createElement('style');
+      this.cbStyle.innerHTML = this.elements.modalStyle;
+      document.head.appendChild(this.cbStyle);
+
+      this.cbIframeStyle = document.createElement('style');
+      this.cbIframeStyle.innerHTML = iframeStyle;
+      document.head.appendChild(this.cbIframeStyle);
+
+      this.cbOpen = document.createElement('div');
+      this.cbOpen.classList.add(this.elements.modalClassNames.open);
+      this.cbOpen.innerHTML = this.elements.modalOpen;
+      document.body.appendChild(this.cbOpen);
+
+      this.cbOverlay = document.createElement('div');
+      this.cbOverlay.classList.add(this.elements.modalClassNames.bg);
+
+      this.cbBody = document.createElement('div');
+      this.cbBody.classList.add(this.elements.modalClassNames.wrap);
+      this.cbBody.innerHTML = this.elements.modalBody;
+
+      this.cbClose = document.createElement('div');
+      this.cbClose.innerHTML = this.elements.modalClose;
+      this.cbBody.querySelector('.br-content').appendChild(this.cbClose);
+
+      this.cbBody.querySelector('#frameForm').src = 'data:text/html;charset=utf-8,' + encodeURI('<link href="https://fonts.googleapis.com/css?family=PT+Sans" rel="stylesheet">' + ('<style>' + iframeStyle + '</style>') + this.elements.modalIframe + ('<script>' + iframeScript + '</script>'));
+      this.cbBody = document.body.insertBefore(this.cbBody, document.body.firstChild);
+      this.cbOverlay = document.body.insertBefore(this.cbOverlay, document.body.firstChild);
+    }
+  }, {
+    key: 'inputListener',
+    value: function inputListener() {
+      // let inputPhone = this.cbBody.querySelector('#frameForm').contentWindow.document.querySelector('#phone');
+      // inputPhone.setAttribute('placeholder',this.options.mask);
+      // this.applyDataMask(inputPhone, this.options.mask);
+    }
+  }, {
+    key: 'open',
+    value: function open() {
+      this.cbBody.style.display = 'block';
+      this.cbOverlay.style.display = 'block';
+    }
+  }, {
+    key: 'close',
+    value: function close() {
+      this.cbBody.style.display = 'none';
+      this.cbOverlay.style.display = 'none';
+    }
+  }, {
+    key: 'setCursor',
+    value: function setCursor(elem, caretPos) {
+      if (elem.createTextRange) {
+        var range = elem.createTextRange();
+        range.move('character', caretPos);
+        range.select();
+      } else {
+        if (elem.selectionStart) {
+          elem.focus();
+          elem.setSelectionRange(caretPos, caretPos);
+        } else elem.focus();
+      }
+    }
+    // applyDataMask(field, mask) {
+    //
+    //   var mask = mask.split('');
+    //
+    //   // For now, this just strips everything that's not a number
+    //   let stripMask = (maskedData) => {
+    //     function isDigit(char) {
+    //       return /\d/.test(char);
+    //     }
+    //     return maskedData.split('').filter(isDigit);
+    //   }
+    //
+    //   // `_` characters => characters from `data`
+    //   let applyMask = (data) =>
+    //     mask.map(function(char, index) {
+    //       if (char != '_' && index != 2) return char;
+    //       if (data.length == 0) return char;
+    //       return data.shift();
+    //     }).join('')
+    //
+    //   let reapplyMask = (data) => applyMask(stripMask(data))
+    //
+    //   let smartfocus = (flag) => {
+    //     let oldStart = field.selectionStart;
+    //     let oldEnd = field.selectionEnd;
+    //     field.value = reapplyMask(field.value);
+    //     if (field.value.indexOf('_') >= 0 && flag) {
+    //       this.setCursor(field, field.value.indexOf('_'));
+    //     } else if (!flag) {
+    //       field.selectionStart = oldStart + 1;
+    //       field.selectionEnd = oldStart;
+    //     } else if (!(field.value.indexOf('_') < 0) && flag) {
+    //       field.selectionStart = oldStart + 1;
+    //       field.selectionEnd = oldStart + 1;
+    //     }
+    //   }
+    //   field.addEventListener('click', (e) => {
+    //     if ( /\d/.test(e.target.value.charAt(field.selectionStart - 1)) ||  /\d/.test(e.target.value.charAt(field.selectionStart))) {
+    //       smartfocus(false)
+    //     } else {
+    //       smartfocus(true)
+    //     }
+    //     if (field.value.indexOf('_') < 0) {
+    //       e.target.classList.add('success')
+    //     } else {
+    //       e.target.classList.remove('success')
+    //     }
+    //   });
+    //   field.addEventListener('keydown', (e) => {
+    //     if (!(e.keyCode >= 48 && e.keyCode <= 57) && e.keyCode !== 8 && e.keyCode !== 46 && !(e.keyCode >= 96 && e.keyCode <= 105) && !(e.keyCode >= 37 && e.keyCode <= 40)) {
+    //       e.preventDefault()
+    //     }
+    //     if (field.value.indexOf('_') < 0) {
+    //       e.target.classList.add('success')
+    //     } else {
+    //       e.target.classList.remove('success')
+    //     }
+    //   });
+    //   field.addEventListener('keyup', (e) => {
+    //     if (e.keyCode == 8 || (e.keyCode >= 37 && e.keyCode <= 40)) {
+    //       smartfocus(false)
+    //     } else if ((e.keyCode >= 48 && e.keyCode <= 57) || (e.keyCode >= 96 && e.keyCode <= 105)) {
+    //       smartfocus(true)
+    //     } else {
+    //       e.preventDefault()
+    //     }
+    //     if (field.value.indexOf('_') < 0) {
+    //       e.target.classList.add('success')
+    //     } else {
+    //       e.target.classList.remove('success')
+    //     }
+    //   })
+    // }
+
+  }]);
+
+  return CallMe;
+}();
--- src/js/callback.js
+++ src/js/callback.js
@@ -1,56 +1,62 @@
 /**
- * Created by BRenat on 10.12.2016.
+ * Created by BRenat on 04.01.2017.
  */
-;
-const CallBack = function(option) {
-  this.option = {};
-  this.option.mask = `+ 7 (___) ___ - __ - __`;
-  this.option.color = {
-      first : '#f49a00',
-      second : '#fab900',
-      phone : '#3CB868'
-  };
-  this.option.modalClassNames = {
-      bg : 'br-bg',
-      wrap : 'br-wrap',
-      container : 'br-container',
-      content : 'br-content',
-      form : 'br-form',
-      open : 'cback'
-  };
-  this.option.imgClose = 'img/close.png';
-  this.option.imgLogo = 'img/logo.png';
-  this.option.modalClose = `<button class="br-close"><img src=${this.option.imgClose} alt=""></button>`;
-  this.option.modalLogo = `<div class="br-logo"><img src=${this.option.imgLogo} alt=""></div>`;
-  this.option.modalText = `<div class="br-text">Закажите обратный звонок, <br/> мы перезвоним Вам в ближайшее время!</div>`;
-  this.option.modalForm = `<form action="#"> \
-                     <div class="br-form__group"> \
-                       <input type="text" placeholder="Ваше имя" title="Введите ваше имя" required="" value="" name="username" id="username" class="br-form__input"> \
-                     </div> \
-                     <div class="br-form__group"> \
-                       <input type="phone" title="Введите ваш номер" required="" value="" name="phone" id="phone" class="br-form__input"> \
-                     </div> \
-                    </form>`;
-  this.option.modalFormBtn = `<button class="br-form__btn">Жду звонка</button>`;
-  this.option.modalOpen = `<div class="cback-circle fn1"></div> \
-                    <div class="cback-circle fn2"></div> \
-                    <div class="cback-circle cback-circle--phone"> \
-                      <i class='phone-icon'></i> \
-                    </div>`
-;
-  this.option = Object.assign({}, this.option, option);
-  this.option.modalStyle = `.cback-circle {color: ${this.option.color.phone}; background: ${this.option.color.phone};} .br-form__btn {background: ${this.option.color.first}} .br-form__btn:hover {background: ${this.option.color.second}} `;
-  console.log(this.option.color.phone)
+const defaults = {
+  imgClose : 'img/close.png',
+  imgLogo : 'http://localhost:8080/img/logo.png',
+  color : {
+    first : '#f49a00',
+    second : '#fab900',
+    phone : '#3CB868'
+  },
+  mask : `+ 7 (___) ___ - __ - __`,
+  position : 'bottom:20px; left:20px;'
 }
 
-CallBack.prototype = {
-  constructor: CallBack,
-  init:function() {
+const iframeStyle = 'body{font-family:PT Sans,sans-serif;margin:0;padding:0}.br-logo{text-align:center}.br-logo img{max-width:100%;max-height:100%}.br-text{text-align:center;padding-top:20px;padding-bottom:25px}.br-form__group{margin-bottom:15px}.br-form__group,.br-form__input{position:relative;text-align:center}.br-form__input{display:block;width:100%;max-width:370px;padding:9px 12px 7px;font-size:25px;line-height:1.42857143;color:#767676;margin:0 auto 20px;background:#fff;box-shadow:0 0 4.8px 0.3px rgba(0,0,0,.2);border:1px solid #ccc;-webkit-transition:all .15s ease-in-out;transition:all .15s ease-in-out;z-index:2}.br-form__input.success{border-color:green}.br-form__input#phone{text-align:left;letter-spacing:2px}.br-form__btn{border-radius:5px;display:block;padding:14px 50px 13px;font-size:18px;font-weight:400;text-align:center;vertical-align:middle;cursor:pointer;-webkit-user-select:none;-moz-user-select:none;-ms-user-select:none;user-select:none;margin:40px auto 10px;color:#fff;background:#3cb868;border:none;text-transform:uppercase;background-size:100% 100%;text-shadow:0 0 4.8px rgba(0,0,0,.2)}';
+
+const iframeScript = `var inputPhone = document.querySelector('#phone');console.log(inputPhone)`;
+
+class CallMe {
+  constructor(options) {
+    this.options = Object.assign({}, defaults, options);
+    this.elements = {
+      modalClassNames : {
+        bg : 'br-bg',
+        wrap : 'br-wrap',
+        container : 'br-container',
+        content : 'br-content',
+        form : 'br-form',
+        open : 'cback'
+      },
+      modalBody: '<div class="br-container"><div class="br-content"><iframe class="br-iframe" name="frameForm" id="frameForm" src="" allowfullscreen></iframe></div></div>',
+      modalClose : `<button class="br-close"><img src=${this.options.imgClose} alt=""></button>`,
+      modalOpen : `<div class="cback-circle fn1"></div><div class="cback-circle fn2"></div><div class="cback-circle cback-circle--phone"><i class='phone-icon'></i></div>`,
+      modalStyle : `.cback-circle {color: ${this.options.color.phone}; background: ${this.options.color.phone};} .br-form__btn {background: ${this.options.color.first}} .br-form__btn:hover {background: ${this.options.color.second}}`,
+      modalIframe : `<div class="br-logo">
+						<img src=${this.options.imgLogo} alt="">
+					</div>
+					<div class="br-text">Закажите обратный звонок, <br> мы перезвоним Вам в ближайшее время!</div>
+					<div class="br-form">
+						<form action="#">
+							<div class="br-form__group">
+								<input type="text" placeholder="Ваше имя" title="Введите ваше имя" required="" value="" name="username" id="username" class="br-form__input">
+							</div>
+							<div class="br-form__group">
+								<input type="phone" title="Введите ваш номер" required="" value="" name="phone" id="phone" class="br-form__input">
+							</div>
+						</form>
+					</div>
+					<button class="br-form__btn">Жду звонка</button>`
+    }
+    this.init()
+  }
+  init() {
     this.render();
     this.clickHandler();
-    this.inputListener();
-  },
-  clickHandler: function() {
+    // this.inputListener();
+  }
+  clickHandler() {
     this.cbOpen.addEventListener('click', (e) => {
       this.open();
       e.preventDefault()
@@ -59,58 +65,51 @@ CallBack.prototype = {
       this.close();
       e.preventDefault()
     });
-  },
-  render: function() {
+  }
+  render() {
     this.cbStyle = document.createElement('style');
-    this.cbStyle.innerHTML = this.option.modalStyle;
+    this.cbStyle.innerHTML = this.elements.modalStyle;
     document.head.appendChild(this.cbStyle);
 
+    this.cbIframeStyle = document.createElement('style');
+    this.cbIframeStyle.innerHTML = iframeStyle;
+    document.head.appendChild(this.cbIframeStyle);
+
+
     this.cbOpen = document.createElement('div');
-    this.cbOpen.classList.add(this.option.modalClassNames.open);
-    this.cbOpen.innerHTML = this.option.modalOpen;
+    this.cbOpen.classList.add(this.elements.modalClassNames.open);
+    this.cbOpen.innerHTML = this.elements.modalOpen;
     document.body.appendChild(this.cbOpen);
 
-
-    this.cbForm = document.createElement('div');
-    this.cbForm.classList.add(this.option.modalClassNames.form);
-    this.cbForm.innerHTML = this.option.modalForm;
-
-    this.cbContent = document.createElement('div');
-    this.cbContent.classList.add(this.option.modalClassNames.content);
-    this.cbContent.innerHTML = this.option.modalLogo + this.option.modalText;
-    this.cbSend = document.createElement('div');
-    this.cbSend.innerHTML = this.option.modalFormBtn;
-    this.cbClose = document.createElement('div');
-    this.cbClose.innerHTML = this.option.modalClose;
-    this.cbContent.appendChild(this.cbForm);
-    this.cbContent.appendChild(this.cbSend);
-    this.cbContent.appendChild(this.cbClose);
-
-    this.cbContainer = document.createElement('div');
-    this.cbContainer.classList.add(this.option.modalClassNames.container);
-    this.cbContainer.appendChild(this.cbContent);
+    this.cbOverlay = document.createElement('div');
+    this.cbOverlay.classList.add(this.elements.modalClassNames.bg);
 
     this.cbBody = document.createElement('div');
-    this.cbBody.classList.add(this.option.modalClassNames.wrap);
-    this.cbBody.appendChild(this.cbContainer);
+    this.cbBody.classList.add(this.elements.modalClassNames.wrap);
+    this.cbBody.innerHTML = this.elements.modalBody;
 
-    this.cbOverlay = document.createElement('div');
-    this.cbOverlay.classList.add(this.option.modalClassNames.bg);
-  },
-  inputListener: function() {
-    let inputPhone = this.cbForm.querySelector('#phone');
-    inputPhone.setAttribute('placeholder',this.option.mask);
-    this.applyDataMask(inputPhone, this.option.mask);
-  },
-  open: function() {
-    document.body.insertBefore(this.cbBody, document.body.firstChild);
-    document.body.insertBefore(this.cbOverlay, document.body.firstChild);
-  },
-  close: function() {
-    this.cbOverlay.remove();
-    this.cbBody.remove();
-  },
-  setCursor: function (elem, caretPos) {
+    this.cbClose = document.createElement('div');
+    this.cbClose.innerHTML = this.elements.modalClose;
+    this.cbBody.querySelector('.br-content').appendChild(this.cbClose);
+
+    this.cbBody.querySelector('#frameForm').src = 'data:text/html;charset=utf-8,' + encodeURI('<link href="https://fonts.googleapis.com/css?family=PT+Sans" rel="stylesheet">'+`<style>${iframeStyle}</style>` + this.elements.modalIframe + `<script>${iframeScript}</script>`);
+    this.cbBody = document.body.insertBefore(this.cbBody, document.body.firstChild);
+    this.cbOverlay = document.body.insertBefore(this.cbOverlay, document.body.firstChild);
+  }
+  inputListener() {
+    // let inputPhone = this.cbBody.querySelector('#frameForm').contentWindow.document.querySelector('#phone');
+    // inputPhone.setAttribute('placeholder',this.options.mask);
+    // this.applyDataMask(inputPhone, this.options.mask);
+  }
+  open() {
+    this.cbBody.style.display = 'block';
+    this.cbOverlay.style.display = 'block';
+  }
+  close() {
+    this.cbBody.style.display = 'none';
+    this.cbOverlay.style.display = 'none';
+  }
+  setCursor(elem, caretPos) {
     if(elem.createTextRange) {
       let range = elem.createTextRange();
       range.move('character', caretPos);
@@ -124,78 +123,78 @@ CallBack.prototype = {
       else
         elem.focus();
     }
-  },
-  applyDataMask: function(field, mask) {
-
-    var mask = mask.split('');
-
-    // For now, this just strips everything that's not a number
-    let stripMask = (maskedData) => {
-      function isDigit(char) {
-        return /\d/.test(char);
-      }
-      return maskedData.split('').filter(isDigit);
-    }
-
-    // `_` characters => characters from `data`
-    let applyMask = (data) =>
-      mask.map(function(char, index) {
-        if (char != '_' && index != 2) return char;
-        if (data.length == 0) return char;
-        return data.shift();
-      }).join('')
-
-    let reapplyMask = (data) => applyMask(stripMask(data))
-
-    let smartfocus = (flag) => {
-      let oldStart = field.selectionStart;
-      let oldEnd = field.selectionEnd;
-      field.value = reapplyMask(field.value);
-      if (field.value.indexOf('_') >= 0 && flag) {
-                this.setCursor(field, field.value.indexOf('_'));
-      } else if (!flag) {
-        field.selectionStart = oldStart + 1;
-        field.selectionEnd = oldStart;
-      } else if (!(field.value.indexOf('_') < 0) && flag) {
-        field.selectionStart = oldStart + 1;
-        field.selectionEnd = oldStart + 1;
-      }
-    }
-    field.addEventListener('click', (e) => {
-      if ( /\d/.test(e.target.value.charAt(field.selectionStart - 1)) ||  /\d/.test(e.target.value.charAt(field.selectionStart))) {
-        smartfocus(false)
-      } else {
-        smartfocus(true)
-      }
-      if (field.value.indexOf('_') < 0) {
-          e.target.classList.add('success')
-      } else {
-          e.target.classList.remove('success')
-      }
-    });
-    field.addEventListener('keydown', (e) => {
-      if (!(e.keyCode >= 48 && e.keyCode <= 57) && e.keyCode !== 8 && e.keyCode !== 46 && !(e.keyCode >= 96 && e.keyCode <= 105) && !(e.keyCode >= 37 && e.keyCode <= 40)) {
-        e.preventDefault()
-      }
-      if (field.value.indexOf('_') < 0) {
-          e.target.classList.add('success')
-      } else {
-          e.target.classList.remove('success')
-      }
-    });
-    field.addEventListener('keyup', (e) => {
-      if (e.keyCode == 8 || (e.keyCode >= 37 && e.keyCode <= 40)) {
-        smartfocus(false)
-      } else if ((e.keyCode >= 48 && e.keyCode <= 57) || (e.keyCode >= 96 && e.keyCode <= 105)) {
-        smartfocus(true)
-      } else {
-        e.preventDefault()
-      }
-      if (field.value.indexOf('_') < 0) {
-          e.target.classList.add('success')
-      } else {
-          e.target.classList.remove('success')
-      }
-    })
   }
+  // applyDataMask(field, mask) {
+  //
+  //   var mask = mask.split('');
+  //
+  //   // For now, this just strips everything that's not a number
+  //   let stripMask = (maskedData) => {
+  //     function isDigit(char) {
+  //       return /\d/.test(char);
+  //     }
+  //     return maskedData.split('').filter(isDigit);
+  //   }
+  //
+  //   // `_` characters => characters from `data`
+  //   let applyMask = (data) =>
+  //     mask.map(function(char, index) {
+  //       if (char != '_' && index != 2) return char;
+  //       if (data.length == 0) return char;
+  //       return data.shift();
+  //     }).join('')
+  //
+  //   let reapplyMask = (data) => applyMask(stripMask(data))
+  //
+  //   let smartfocus = (flag) => {
+  //     let oldStart = field.selectionStart;
+  //     let oldEnd = field.selectionEnd;
+  //     field.value = reapplyMask(field.value);
+  //     if (field.value.indexOf('_') >= 0 && flag) {
+  //       this.setCursor(field, field.value.indexOf('_'));
+  //     } else if (!flag) {
+  //       field.selectionStart = oldStart + 1;
+  //       field.selectionEnd = oldStart;
+  //     } else if (!(field.value.indexOf('_') < 0) && flag) {
+  //       field.selectionStart = oldStart + 1;
+  //       field.selectionEnd = oldStart + 1;
+  //     }
+  //   }
+  //   field.addEventListener('click', (e) => {
+  //     if ( /\d/.test(e.target.value.charAt(field.selectionStart - 1)) ||  /\d/.test(e.target.value.charAt(field.selectionStart))) {
+  //       smartfocus(false)
+  //     } else {
+  //       smartfocus(true)
+  //     }
+  //     if (field.value.indexOf('_') < 0) {
+  //       e.target.classList.add('success')
+  //     } else {
+  //       e.target.classList.remove('success')
+  //     }
+  //   });
+  //   field.addEventListener('keydown', (e) => {
+  //     if (!(e.keyCode >= 48 && e.keyCode <= 57) && e.keyCode !== 8 && e.keyCode !== 46 && !(e.keyCode >= 96 && e.keyCode <= 105) && !(e.keyCode >= 37 && e.keyCode <= 40)) {
+  //       e.preventDefault()
+  //     }
+  //     if (field.value.indexOf('_') < 0) {
+  //       e.target.classList.add('success')
+  //     } else {
+  //       e.target.classList.remove('success')
+  //     }
+  //   });
+  //   field.addEventListener('keyup', (e) => {
+  //     if (e.keyCode == 8 || (e.keyCode >= 37 && e.keyCode <= 40)) {
+  //       smartfocus(false)
+  //     } else if ((e.keyCode >= 48 && e.keyCode <= 57) || (e.keyCode >= 96 && e.keyCode <= 105)) {
+  //       smartfocus(true)
+  //     } else {
+  //       e.preventDefault()
+  //     }
+  //     if (field.value.indexOf('_') < 0) {
+  //       e.target.classList.add('success')
+  //     } else {
+  //       e.target.classList.remove('success')
+  //     }
+  //   })
+  // }
 }
